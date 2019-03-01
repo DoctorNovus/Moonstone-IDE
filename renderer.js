90e9f8502c289a6e782fbb3288cb97968a4e6c70
Justin <22105395+ProjectXTeam@users.noreply.github.com>
2019-02-28 22:05:55 -0700

    Add files via upload

--- renderer.js
+++ renderer.js
@@ -16,7 +16,6 @@ function setMainMenu() {
                     if(fileName == undefined){
                         return;
                     }
-            
                     fs.writeFile(fileName, content.value, (err) => {
                         if (err) console.log(err);
                     });
@@ -32,6 +31,7 @@ function setMainMenu() {
                         return;
                     } else {
                         function readFile(filepath){
+                            fileName.innerHTML = filepath;
                             fs.readFile(filepath, 'utf-8', (err, data) => {
                                 if(err){
                                     return;
@@ -40,16 +40,15 @@ function setMainMenu() {
                                 function countLine(element) {
                                     lineCount.innerHTML = ""
                                     var text = document.getElementById(element).value;
-                                    var lines = text.split("\n");
+                                    var lines = text.split("\n")
                                     var count = 1;
                                     for (var i = 0; i < lines.length-1; i++) {
-                                        lineCount.innerHTML += "<br>" + count;
+                                        lineCount.innerHTML += "\n" + count;
                                         count += 1;
                                     }
-                                    lineCount.innerHTML += "<br>" + count;
+                                    lineCount.innerHTML += "\n" + count;
                                 }
-                                
-                                content.value = data;
+                                content.innerHTML = data;
                         
                                 console.log(countLine('fileContent'));
                             });
@@ -73,16 +72,33 @@ var fs = require('fs');
 
 let content = document.getElementById('fileContent');
 let lineCount = document.getElementById('lineCount');
+let fileName = document.getElementById('fileName');
 
 lineCount.style.fontSize = content.style.fontSize;
-content.style.width = window.innerWidth - 35 + "px";
-content.style.left = 35 + "px";
+content.style.width = window.innerWidth - 40 + "px";
+content.style.left = 40 + "px";
 content.style.height = 400 + "px";
 lineCount.style.width = 34 + "px";
-lineCount.style.height = 405 + "px";
+lineCount.style.height = content.offsetHeight - 5 + "px";
 lineCount.style.top = content.offsetTop + "px";
-lineCount.style.left = content.offsetLeft - 35 + "px";
+lineCount.style.left = content.offsetLeft - 40 + "px";
 
 content.addEventListener('scroll', () => {
    lineCount.scrollTop = content.scrollTop; 
+});
+
+content.addEventListener('keydown', () => {
+    function countLine(element) {
+        lineCount.innerHTML = ""
+        var text = document.getElementById(element).value;
+        var lines = text.split("\n")
+        var count = 1;
+        for (var i = 0; i < lines.length-1; i++) {
+            lineCount.innerHTML += "\n" + count;
+            count += 1;
+        }
+        lineCount.innerHTML += "\n" + count;
+    }
+
+    console.log(countLine('fileContent'))
 });
